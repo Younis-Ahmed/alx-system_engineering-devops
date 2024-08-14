9fd6315295572e1ff89e7909c41ff00914d94ff0
Younis <23105954+Younis-Ahmed@users.noreply.github.com>
2024-08-14 05:09:43 +0000

    Refactor variable name in 0x16-api_advanced/0-subs.py for clarity and consistency

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"function that queries the Reddit API and returns the number of subscribers"
+"""function that queries the Reddit API and returns the number of subscribers
+for a given subreddit"""
 import requests
 
 
